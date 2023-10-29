80cc705349495f1b396bb9cd0038a68d66fc971e
polinavishnev <polina@uni.minerva.edu>
2023-10-28 20:35:11 -0700

    code to add 20 latest mail snippets to cockroach

--- quickstart.py
+++ quickstart.py
@@ -8,14 +8,42 @@ from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
 
+import logging
+import os
+import random
+import time
+import uuid
+from argparse import ArgumentParser, RawTextHelpFormatter
+
+import psycopg
+from psycopg.errors import SerializationFailure, Error
+from psycopg.rows import namedtuple_row
+
+from dotenv import load_dotenv
+
 # If modifying these scopes, delete the file token.json.
+
+import psycopg2
+from psycopg2.extras import execute_values
+
+import os
+
 SCOPES = ['https://www.googleapis.com/auth/gmail.readonly']
+load_dotenv()
+conn = psycopg2.connect(os.environ['DATABASE_URL'], sslmode='verify-full', sslrootcert='system')
+
+# write a function to use the connection to list tables
+def list_tables():
+    with conn.cursor() as cur:
+        cur.execute("SELECT table_name FROM information_schema.tables WHERE table_schema = 'public' ORDER BY table_name;")
+        print(cur.fetchall())
 
 
 def main():
     """Shows basic usage of the Gmail API.
     Lists the user's Gmail labels.
     """
+    list_tables()
     creds = None
     # The file token.json stores the user's access and refresh tokens, and is
     # created automatically when the authorization flow completes for the first
@@ -37,15 +65,28 @@ def main():
     try:
         # Call the Gmail API
         service = build('gmail', 'v1', credentials=creds)
-        results = service.users().labels().list(userId='me').execute()
-        labels = results.get('labels', [])
+        results = service.users().messages().list(userId='me', maxResults=20).execute()
+        messages = results.get('messages', [])
 
-        if not labels:
-            print('No labels found.')
+        if not messages:
+            print('No messages found.')
             return
-        print('Labels:')
-        for label in labels:
-            print(label['name'])
+        print('Messages:')
+        values = []
+        id = 0
+
+        for msg in messages:
+            message = service.users().messages().get(userId='me', id=msg['id']).execute()
+            snpt = message['snippet'][:255] # TODO: make this a config var
+            values.append((id, snpt))
+            id += 1
+
+        with conn.cursor() as cur:
+            execute_values(cur, "INSERT INTO emails (id, snippet) VALUES %s ON CONFLICT (id) DO NOTHING;", values)
+            conn.commit()
+            cur.execute("SELECT * FROM emails;")
+            print(cur.fetchall())
+        
 
     except HttpError as error:
         # TODO(developer) - Handle errors from gmail API.
