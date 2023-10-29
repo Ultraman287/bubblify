017cac8eea205ffff7c5a04be0afef59754d62d3
polinavishnev <polina@uni.minerva.edu>
2023-10-28 22:55:46 -0700

    inserting values into the database

--- quickstart.py
+++ quickstart.py
@@ -7,6 +7,7 @@ from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
+import datetime
 
 import logging
 import os
@@ -38,6 +39,51 @@ def list_tables():
         cur.execute("SELECT table_name FROM information_schema.tables WHERE table_schema = 'public' ORDER BY table_name;")
         print(cur.fetchall())
 
+def create_categories(conn, labels):
+    with conn.cursor() as cur:
+        category_ids = []  # Store category IDs
+
+        for label in labels:
+            # Check if the category already exists in the database
+            cur.execute("SELECT id FROM categories WHERE name = %s;", (label,))
+            category_id = cur.fetchone()
+            
+            if category_id is None:
+                # If the category doesn't exist, insert it into the categories table
+                cur.execute("INSERT INTO categories (name) VALUES (%s) RETURNING id;", (label,))
+                category_id = cur.fetchone()[0]
+            else:
+                category_id = category_id[0]
+            
+            category_ids.append(category_id)  # Append the category ID to the list
+
+        conn.commit()
+
+    return category_ids
+
+
+
+def insert_email_info(conn, data):
+    with conn.cursor() as cur:
+        insert_query = """
+        INSERT INTO emails_info (snippet, unread, subject, sender, date_received) 
+        VALUES %s 
+        ON CONFLICT (id) DO NOTHING
+        RETURNING id;
+        """
+        psycopg2.extras.execute_values(cur, insert_query, data)
+        email_info_ids = cur.fetchall()
+        conn.commit()
+    return email_info_ids
+
+
+def insert_categorized_email(conn, email_info_id, category_id):
+    with conn.cursor() as cur:
+        cur.execute("INSERT INTO categorized_emails (email_id, category_id) VALUES (%s, %s);", (email_info_id, category_id))
+        conn.commit()
+
+
+
 
 def main():
     """Shows basic usage of the Gmail API.
@@ -65,32 +111,77 @@ def main():
     try:
         # Call the Gmail API
         service = build('gmail', 'v1', credentials=creds)
-        results = service.users().messages().list(userId='me', maxResults=20).execute()
+        results = service.users().messages().list(userId='me', maxResults=5).execute()
         messages = results.get('messages', [])
 
         if not messages:
             print('No messages found.')
             return
         print('Messages:')
-        values = []
-        id = 0
+        
+        email_info_data = []
 
         for msg in messages:
             message = service.users().messages().get(userId='me', id=msg['id']).execute()
-            snpt = message['snippet'][:255] # TODO: make this a config var
-            values.append((id, snpt))
-            id += 1
+            
+            
+            # Extract the date
+            date = message['internalDate']
+            date = datetime.datetime.fromtimestamp(int(date) / 1000).strftime('%Y-%m-%d %H:%M:%S')
 
-        with conn.cursor() as cur:
-            execute_values(cur, "INSERT INTO emails (id, snippet) VALUES %s ON CONFLICT (id) DO NOTHING;", values)
-            conn.commit()
-            cur.execute("SELECT * FROM emails;")
-            print(cur.fetchall())
+            # Check if the email is unread
+            is_unread = 'UNREAD' in message['labelIds']
+
+
+            subject = None
+            for header in message['payload']['headers']:
+                if header['name'] == 'Subject':
+                    subject = header['value']
+                    break
+
+            # Extract the sender
+            sender = None
+            for header in message['payload']['headers']:
+                if header['name'] == 'From':
+                    sender = header['value']
+                    break
+
+            # Extract the snippet
+            snpt = message['snippet'][:255]  # TODO: make this a config var
+
+            # Extract all label tags
+            label_tags = [label for label in message['labelIds'] if label != 'UNREAD']
+            
+            email_info_data.append((snpt, is_unread, subject, sender, date))
+
+        # Insert data into the emails_info table using execute_values
+        email_info_ids = insert_email_info(conn, email_info_data)
+
+        # Create categories for labels
+        category_ids = create_categories(conn, label_tags)
+
+
+        for idx, email_info_id in enumerate(email_info_ids):
+        # Insert data into the categorized_emails table
+        # For simplicity, let's assume you associate each email with the first category
+            if idx < len(category_ids):
+                category_id = category_ids[idx]
+            else:
+                # If there are more emails than categories, use the last category (or any default behavior)
+                category_id = category_ids[-1]
+
+            # Insert data into the categorized_emails table
+            insert_categorized_email(conn, email_info_id, category_id)
+
+
         
 
     except HttpError as error:
         # TODO(developer) - Handle errors from gmail API.
         print(f'An error occurred: {error}')
+
+
+
 
 
 if __name__ == '__main__':
